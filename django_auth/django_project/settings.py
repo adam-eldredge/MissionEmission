24399fe60480785b77d3add651fa2be32339e85a
jontrodrigues <rodriguesj@ufl.edu>
2022-11-29 22:30:21 -0500

    changed the setting.py to write to online database instead of virtual. writes to an amazon rds server running postgresql

--- django_auth/django_project/settings.py
+++ django_auth/django_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "MEDatabase",
+        "USER": "postgres",
+        "PASSWORD": "team20cen3031",
+        "HOST": "team20data.czs9w6dbrxsp.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
